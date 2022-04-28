84e86232ea7ed84e60b682a2fc4d13aa8b30c2cb
Basavaraj Vijapure <basavavijapure0206@gmail.com>
2022-04-28 23:39:53 +0530

    adding changes in java as well as txt files

--- src/main/java/com/application/Demo.java
+++ src/main/java/com/application/Demo.java
@@ -3,11 +3,14 @@ package com.application;
 /**
  * 
  * @author Basavaraj Vijapure
+ * This is Demo Java
  *
  */
 public class Demo {
 
 	public static void main(String[] args) {
+	
+		Demo d1 = new Demo();
 		
 	}
 }
